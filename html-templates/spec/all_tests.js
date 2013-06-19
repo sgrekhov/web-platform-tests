1a4c8858e44167a2a747511830915e01a99e6261
Sergey G. Grekhov <sgrekhov@unipro.ru>
2013-06-19 12:45:02 +0700

    Spec coverage tool. Initial commit

---
+++ html-templates/spec/all_tests.js
@@ -0,0 +1,15 @@
+
+include("../test/05-definitions/template-contents-owner-document-type.js");
+include("../test/05-definitions/template-contents-owner-test-01.js");
+include("../test/05-definitions/template-contents-owner-test-02.js");
+include("../test/05-definitions/template-contents.js");
+include("../test/06-template-element/content-attribute.js");
+include("../test/06-template-element/node-document-changes.js");
+include("../test/06-template-element/node-document.js");
+include("../test/06-template-element/template-content-body.js");
+include("../test/06-template-element/template-content-frameset.js");
+include("../test/06-template-element/template-content-head.js");
+include("../test/06-template-element/template-content-html.js");
+include("../test/06-template-element/template-content.js");
+include("../test/12-additions-to-parsing-xhtml-documents/node-document.js");
+include("../test/12-additions-to-parsing-xhtml-documents/template-child-nodes.js");
