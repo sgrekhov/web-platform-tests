0e7d1b3cdedb44f73d0b15b019f6765e3dc4bbf9
Sergey G. Grekhov <sgrekhov@unipro.ru>
2014-05-15 00:06:17 +0700

    Current time calculation functions removed and some methods renamed

--- web-animations/testcommon.js
+++ web-animations/testcommon.js
@@ -10,6 +10,7 @@ policies and contribution forms [3].
 
 "use strict";
 
+// Creates and returns new HTML document
 function newHTMLDocument() {
     return document.implementation.createHTMLDocument('Test Document');
 }
@@ -47,6 +48,7 @@ function testInBlankDocumentWithBrowsingContext(f, testName, testProps) {
     testInIFrame('../resources/blank.html', f, testName, testProps);
 }
 
+
 function newAnimation(animationTarget) {
     var animation = new Animation(animationTarget, 
             [{top: "0px", offset: 0}, 
@@ -68,80 +70,28 @@ function newAnimationPlayer(doc) {
 function isCurrent(timedItem) {
     // FIXME Add the third condition:
     // it has a parent animation group and the parent animation group is current.
-    return isBeforePhase(timedItem) || isPlayPhase(timedItem);
+    return isInBeforePhase(timedItem) || isInPlay(timedItem);
 }
 
 // Returns true if timed item is in before phase as defined at
 // http://dev.w3.org/fxtf/web-animations/#dfn-before-phase
-function isBeforePhase(timedItem) {
+function isInBeforePhase(timedItem) {
     return timedItem.localTime != null && timedItem.startTime < timedItem.timing.delay;
 }
 
-// Returns true if timed item is in play phase as defined at
+// Returns true if timed item is in play as defined at
 // http://dev.w3.org/fxtf/web-animations/#dfn-in-play
-function isPlayPhase(timedItem) {
+function isInPlay(timedItem) {
     // FIXME Add the second condition:
     // 2. the timed item has a parent animation group
     // that is in play or else is directly associated with a player that is not limited.
-    return isActivePhase(timedItem);
+    return isInActivePhase(timedItem);
 }
 
 // Returns true if timed item is in active phase as defined at
 // http://dev.w3.org/fxtf/web-animations/#dfn-active-phase
-function isActivePhase(timedItem) {
+function isInActivePhase(timedItem) {
     return timedItem.localTime != null &&
         timedItem.startTime >= timedItem.timing.delay &&
         timedItem.startTime <= timedItem.timing.delay + timedItem.activeDuration;
-}
-
-
-// TODO Revisit all of the methods below. If they won't be used then remove them
-
-// Calculates Current time as described at
-// http://dev.w3.org/fxtf/web-animations/#the-current-time-of-a-player
-function calculateCurrentTime(timelineTime, adjustedStartTime, playbackRate, holdTime) {
-    if (holdTime == null) {
-        return (timelineTime - adjustedStartTime) * playbackRate;
-    } else {
-        return holdTime;
-    }
-}
-
-// Returns animation player adjusted start time as described at
-// http://dev.w3.org/fxtf/web-animations/#calculating-the-adjusted-start-time
-function getAdjustedTime(player) {
-    if (player.paused) {
-        return player.timeline.currentTime - getHoldTime(player) / player.playbackRate;
-    } else {
-        // FIXME How to get stored value for the adjusted start time?
-        return player.startTime;
-    }
-}
-
-// Returns Effective Timeline Time as described at
-// http://dev.w3.org/fxtf/web-animations/#dfn-effective-timeline-time
-function getEffectiveTimelineTime(player) {
-    if (player.timeline.currentTime != null) {
-        return player.timeline.currentTime;
-    }
-    return 0;
-}
-
-// Retuns Effective Current Time as described at
-// http://dev.w3.org/fxtf/web-animations/#dfn-effective-current-time
-function getEffectiveCurrentTime(player) {
-    return calculateCurrentTime(getEffectiveTimelineTime(player), getAdjustedTime(player),
-        player.playbackRate, getHoldTime(player));
-}
-
-// Returns player hold time as described at
-// http://dev.w3.org/fxtf/web-animations/#dfn-hold-time
-function getHoldTime(player) {
-    var holdTime = null;
-    if (player.paused) {
-        holdTime = getEffectiveCurrentTime(player);
-    }
-    // FIXME Shouldn't the conditions for limited behaviour and automaticaly updationg
-    // of the hold time be applied here?
-    return holdTime;
 }
